8803f4dddb1a3e911881cae7ae3987652569cf21
gawasa29 <yusei112929@gmail.com>
2025-04-27 15:52:00 +0900

    complete Chapter13

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -26,10 +26,15 @@ export async function createInvoice(formData: FormData) {
   const amountInCents = amount * 100
   const date = new Date().toISOString().split("T")[0]
 
-  await sql`
+  try {
+    await sql`
       INSERT INTO invoices (customer_id, amount, status, date)
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `
+  } catch (error) {
+    // We'll log the error to the console for now
+    console.error(error)
+  }
 
   revalidatePath("/dashboard/invoices")
   redirect("/dashboard/invoices")
@@ -37,8 +42,6 @@ export async function createInvoice(formData: FormData) {
 
 // Use Zod to update the expected types
 const UpdateInvoice = FormSchema.omit({ id: true, date: true })
-
-// ...
 
 export async function updateInvoice(id: string, formData: FormData) {
   const { customerId, amount, status } = UpdateInvoice.parse({
@@ -49,17 +52,21 @@ export async function updateInvoice(id: string, formData: FormData) {
 
   const amountInCents = amount * 100
 
-  await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `
+  try {
+    await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+      `
+  } catch (error) {
+    // We'll log the error to the console for now
+    console.error(error)
+  }
 
   revalidatePath("/dashboard/invoices")
   redirect("/dashboard/invoices")
 }
 
 export async function deleteInvoice(id: string) {
-  await sql`DELETE FROM invoices WHERE id = ${id}`
-  revalidatePath("/dashboard/invoices")
+  throw new Error("Failed to Delete Invoice")
 }
